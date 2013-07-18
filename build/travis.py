d2ec1ec0808600d5b99e5698d0e1c25701d5b066
Alan Shreve <alan@inconshreveable.com>
2013-07-18 18:24:55 +0100

    fix windows build with exe extension

--- build/travis.py
+++ build/travis.py
@@ -6,12 +6,15 @@ access_key = os.getenv("AWS_ACCESS_KEY")
 secret_key = os.getenv("AWS_SECRET_KEY")
 bucket = os.getenv("BUCKET")
 version = os.getenv("VERSION")
+os = os.getenv("GOOS")
 
 s3 = boto.s3.connection.S3Connection(access_key, secret_key)
 bucket = s3.get_bucket(bucket)
 
 for envpath in ["NGROK", "NGROKD"]:
 	file_path = os.getenv(envpath)
+	if os == "windows":
+		file_path += ".exe"
 	dir_path, name = os.path.split(file_path)
 	_, platform = os.path.split(dir_path)
 	key_name = "%s/%s/%s" % (platform, version, name)
