22537c01364b867d0ba335e6024242752a9bda47
Alan Shreve <alan@inconshreveable.com>
2013-09-20 19:41:31 +0300

    add bug reporting instructions on crash

--- src/ngrok/util/trace.go
+++ src/ngrok/util/trace.go
@@ -5,8 +5,17 @@ import (
 	"runtime"
 )
 
+const crashMessage = `Oh noes! ngrok crashed!
+
+Please submit the stack trace and any relevant information to:
+github.com/inconshreveable/ngrok/issues
+
+panic: %v
+
+%s`
+
 func MakePanicTrace(err interface{}) string {
 	stackBuf := make([]byte, 4096)
 	n := runtime.Stack(stackBuf, false)
-	return fmt.Sprintf("panic: %v\n\n%s", err, stackBuf[:n])
+	return fmt.Sprintf(crashMessage, err, stackBuf[:n])
 }
