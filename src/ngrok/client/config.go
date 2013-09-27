9de09f3f1d1ee48c5367f6bf35e35430c0560210
Alan Shreve <alan@inconshreveable.com>
2013-09-27 21:24:50 +0200

    fix a bug where your auth token couldn't be saved to a configuration file if no configuration file existed

--- src/ngrok/client/config.go
+++ src/ngrok/client/config.go
@@ -232,15 +232,16 @@ func validateProtocol(proto, propName string) (err error) {
 }
 
 func SaveAuthToken(configPath, authtoken string) (err error) {
+	// empty configuration by default for the case that we can't read it
+	c := new(Configuration)
+
 	// read the configuration
 	oldConfigBytes, err := ioutil.ReadFile(configPath)
-	if err != nil {
-		return
-	}
-
-	c := new(Configuration)
-	if err = goyaml.Unmarshal(oldConfigBytes, c); err != nil {
-		return
+	if err == nil {
+		// unmarshal if we successfully read the configuration file
+		if err = goyaml.Unmarshal(oldConfigBytes, c); err != nil {
+			return
+		}
 	}
 
 	// no need to save, the authtoken is already the correct value
