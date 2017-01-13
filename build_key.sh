b18194b3a4da219c29cfaf6e32324e0373c32332
chucklee <sapronlee@gmail.com>
2017-01-13 23:37:09 +0800

    添加两个脚本，用于生成证书和编译文件

---
+++ build_key.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+export NGROK_DOMAIN=ngrok.lxwgo.com
+openssl genrsa -out rootCA.key 2048
+
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+
+openssl genrsa -out device.key 2048
+
+openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
+
+openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+
+cp rootCA.pem assets/client/tls/ngrokroot.crt
+cp device.crt assets/server/tls/snakeoil.crt
+cp device.key assets/server/tls/snakeoil.key
+
+rm -rf device*
+rm -rf rootCA*
